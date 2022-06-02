f944722043be9ed583b98fd9599c3a3c0625f497
frankmutinda <hlrymutinda@gmail.com>
2022-06-02 17:22:29 +0300

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,14 +10,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t file, fread, fwrite;
 char *totalSize;
-
 totalSize = malloc(sizeof(char) * letters);
 if (totalSize == NULL)
 return (0);
 if (filename == NULL)
 return (0);
 file = open(filename, O_RDONLY);
-    
 if (file == -1)
 return (0);
 fread = read(file, totalSize, letters);
